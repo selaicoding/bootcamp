558415065425eb17dd7d040ea9c3fcdb3d80357f
selaicoding <rahardiantorizki@gmail.com>
2025-01-25 00:04:36 +0800

    menambahkan route checkout dan success checkout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Facade\FlareClient\View;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('login', function(){
     return view('login');
 })->name('login');
+
+Route::get('checkout', function() {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('succes-checkout', function(){
+    return view('success');
+})->name('success-checkout');
